941309375688aeab3f640a9325744aeacae30f9a
“darek632” <162553961+darek632@users.noreply.github.com>
2024-06-20 08:38:24 +0200

    Create a full 16x16 grid with border using loops
    Using one loop first to create one horizontal
    container with 16 small grid boxes and append to
    main container.
    
    Using the second loop to loop the first loop 16
    times create a square grid.

--- script.js
+++ script.js
@@ -3,21 +3,30 @@ container.setAttribute(
     "style", "border: 1px solid black; display: inline-block;"
 );
 
-const lineBox = document.createElement("div");
+   
+
+function oneLine () {
+    const lineBox = document.createElement("div");
     lineBox.classList.add("line");
-    lineBox.setAttribute(
-        "style", "display:flex;"
-    );
-    container.appendChild(lineBox);
-
-    
+    lineBox.setAttribute("style", "display:flex;");         
+container.appendChild(lineBox);
 
 
 
+    for (let j=0; j<16; j++) { 
+    const smallBox = document.createElement("div");
+smallBox.setAttribute(
+"style", "height: 20px; width: 20px; border: 1px solid red;");
+lineBox.appendChild(smallBox);
+}
+};
 
-const smallBox = document.createElement("div");
-    smallBox.setAttribute(
-        "style", "height: 20px; width: 20px; border: 1px solid red;"
-    );
-    lineBox.appendChild(smallBox);
+
+function fullGrid () { 
+    for (i=0; i<16; i++) {
+        oneLine()
+    }
+}
+
+fullGrid();
 
