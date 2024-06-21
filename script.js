b9a0861c56e02125bb0b0cf64b00956805713a46
“darek632” <162553961+darek632@users.noreply.github.com>
2024-06-21 16:14:34 +0200

    Add logic for variation of grid boxes
    The container remains the same size, but the
    number of grid elements on each side changes
    dependent on the n variables. Still need to
    incorporate this into the button click and
    receiving the prompt from the user.

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const container = document.querySelector("#container");
 container.setAttribute(
-    "style", "border: 1px solid black; display: inline-block;"
-);
+    "style","border: 1px solid black; display: inline-block;height:640px;width: 640px;");
 
    
 
 function oneLine () {
+    let n = 16;
     const lineBox = document.createElement("div");
     lineBox.classList.add("line");
-    lineBox.setAttribute("style", "display:flex;");         
+    lineBox.setAttribute("style", "display:flex; flex: 1 1 100%; flex-wrap: wrap;");
+    lineBox.style.height = "calc(100%/n)" ;     
 container.appendChild(lineBox);
 
 
 
-    for (let j=0; j<16; j++) { 
+    for (let j=0; j<n; j++) { 
     const smallBox = document.createElement("div");
 smallBox.setAttribute(
-"style", "height: 20px; width: 20px; border: 1px solid red;");
+"style", "flex: 1 1 0;  aspect-ratio: 1/1;  box-sizing: border-box; border: 1px solid red;");
 smallBox.addEventListener("mouseenter", enter);
 lineBox.appendChild(smallBox);
 
@@ -29,12 +30,14 @@ function enter (event)  {
 }; 
 
 function fullGrid () { 
-    for (i=0; i<16; i++) {
+    let n = 16;
+    for (i=0; i<n; i++) {
         oneLine();
     }
  } ;
 
 fullGrid();
+
 
 
 
