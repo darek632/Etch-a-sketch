84dcb2cb6fdbe18c8476ab98a8413045ab83954f
“darek632” <162553961+darek632@users.noreply.github.com>
2024-06-24 14:48:29 +0200

    Add correct logic for user input selection
    Linked functions to remove old grid and create
    a new one in the container, based on user input.
    
    The do while loop ensures that:
    - if a user presses OK without entering any text
    or presses Cancel, this will provide empty string
    or null respetively.
    - The loop will exit and stop asking the user,
    avoiding the infinity loop i previously created.
    - Using Number function on the input, if user
    types string, the number function provides NaN.
    Otherwise, will provide 0 or a number.
    - Included a conditional that checks if the input
    is within the range and is not NaN!

--- script.js
+++ script.js
@@ -2,17 +2,44 @@ const container = document.querySelector("#container");
 container.setAttribute(
     "style","border: 1px solid black; display: inline-block;height:640px;width: 640px;");
 
-   
+    function updateGrid () { 
+
+        let allLines= document.querySelectorAll(".line");
+        allLines.forEach(line => line.remove());
+        
+
+    function grabInput () {
+        let selection ;
+
+        do  {
+
+            selection = (prompt("Pick a number of boxes to have per side.","Between 0 and 100"));
+
+            if (selection === null || selection === '') { 
+                return null;
+            }
+
+            selection = Number(selection);
+        }
+
+        while (isNaN(selection) || selection <= 0 || selection > 100);
+
+        return +selection;
+    
+        }
+
+    
+    
+     let userInput = grabInput();
+     console.log( userInput);
 
 function oneLine () {
-    let n=16;
+    let n=userInput; 
     const lineBox = document.createElement("div");
     lineBox.classList.add("line");
     lineBox.setAttribute("style", "display:flex; flex: 1 1 100%; flex-wrap: wrap;");
     lineBox.style.height = "calc(100%/n)" ;     
 container.appendChild(lineBox);
-
-
 
     for (let j=0; j<n; j++) { 
     const smallBox = document.createElement("div");
@@ -30,30 +57,44 @@ function enter (event)  {
 }; 
 
 function fullGrid () { 
-    let n = 16;
+   let n = userInput;
     for (i=0; i<n; i++) {
         oneLine();
     }
  } ;
 
- fullGrid();
-
-function getGridSize() { 
-    let input;
-
-    while (input === '' || input === null || input === undefined || typeof input !== 'number'|| 0 < input < 100 ) { 
-        input = prompt("Please enter a number to determine number of boxes per size",
-            "Between 0 and 100");
-       
-    }
-
-    return input;
-}
-
-    const userInput = getGridSize();
+fullGrid();
 
 
-    
+ }
+
+document.querySelector("#generator").addEventListener("click",updateGrid);
+
+
+let iAmNull = null;
+console.log(Number(iAmNull));
+
+let iAmUndefined = undefined;
+console.log(Number(iAmUndefined));
+console.log(isNaN(iAmUndefined));
+
+let iAmEmpty = '';
+console.log(Number(iAmEmpty));
+
+let iAmString = 'twetny is plenty diked';
+console.log(Number(iAmString));
+
+
+ // pseudo for button prompt
+ // create button for generating number of squares per side
+ // create function to ask user for input
+ // input must be a value between 0 and 100 
+ // if input null,undefined,empty string or NaN ask again.
+ // use return value of prompt to be the 'n' in function oneLine
+ // use return value of prompt to also be the 'n' in function fullGrid
+ // 
+
+
 
 
 
